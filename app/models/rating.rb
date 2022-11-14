882127fa62278d97dba4d2ba2557df301c9767ad
Petri <yogho@hotmail.com>
2022-11-14 18:33:36 +0200

    t 2.3

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,3 +1,10 @@
 class Rating < ApplicationRecord
     belongs_to :beer
+
+    def to_s
+        beer_name = self.beer.name
+        score = self.score
+        "#{beer_name} #{score}"
+    end
+
 end
