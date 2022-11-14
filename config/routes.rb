b229dda3ceebd5c8bd6fd2a727f4059db3b66d23
Petri <yogho@hotmail.com>
2022-11-14 21:01:08 +0200

    t 2.7

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root "breweries#index"
   get "ratings", to: "ratings#index"
+  get "ratings/new", to: "ratings#new"
+  post "ratings", to:"ratings#create"
   resources :beers
   resources :breweries
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
